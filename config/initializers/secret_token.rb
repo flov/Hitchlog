7e5a4c8240a8240ddfd2653ba22ada9621f04e1f
flov <florian.vallen@gmail.com>
2010-10-14 13:59:18 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaperclipOnHeroku::Application.config.secret_token = 'e10b7d51c20294406a665e61a9ede1d13a02b9a285835173ca09b7ee7670b1a66befa18c284a7a465fe941125a167e807b64c45162d62e9bb91fb30e250038f0'
