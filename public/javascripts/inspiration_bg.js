8f160505838ef57b0b56c0c5ef648144d319c9d4
flov <florian.vallen@gmail.com>
2010-11-23 18:51:53 +0000

    removing background color, making the design more simple with black and white fonts and backgroundss

--- public/javascripts/inspiration_bg.js
+++ public/javascripts/inspiration_bg.js
@@ -1,7 +1,2 @@
 // I get the day of the month to fill in the background image with the day's picture from http://inspiration.sweyla.com/
 // it changes every hour
-$(
-	function(){
-    var today=new Date()
-    $('body').css('background', 'url(http://inspiration.sweyla.com/image/'+today.getDate()*today.getYear()*today.getHours()+'.png)')
-})
