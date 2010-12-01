d79ed026042f766a62258625c4b1cb32bd252a43
flov <florian.vallen@gmail.com>
2010-12-01 18:04:57 +0000

    tests passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,4 @@ require 'spec_helper'
 describe User, 'valid' do
   it { should have_many(:hitchhikes) }
   it { should validate_presence_of(:username) }
-  it { should validate_uniqueness_of(:username) }
 end
