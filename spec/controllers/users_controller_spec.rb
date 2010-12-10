7b1c560acd4c0d8dfb17cafd7b032a80dcfaa82a
flov <florian.vallen@gmail.com>
2010-12-10 11:19:49 +0100

    added rides to user show page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
-describe UsersController do
-
+describe UsersController, '#show' do
+  it do
+    @trip = Factory.create :trip
+    2.times{Factory.create :hitchhike}
+    get 'show'
+    
+    response.should_be success
+  end
 end
