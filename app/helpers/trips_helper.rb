a86bcd84e383f9277f4ed8f102b1f50ac9b0a323
flov <florian.vallen@gmail.com>
2010-12-20 15:53:08 +0100

    much cooler date view for trips

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -1,2 +1,12 @@
 module TripsHelper
+  def trip_date(trip)
+    @month_array ||= []
+    if !trip.date.nil? && !@month_array.include?(trip.date.strftime("%B%y"))
+      @month_array << trip.date.strftime("%B%y")
+      date = trip.date.strftime("<h3 class='date'>%B<strong>%y</strong></h3>").html_safe unless trip.date.nil?
+    else
+      date = ''
+    end
+    date
+  end
 end
