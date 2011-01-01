bfe70df6e83bceb33d531eac62ac308c437b8561
flov <florian.vallen@gmail.com>
2011-01-01 19:05:34 +0100

    hitchhike.me supports page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,5 +5,6 @@ class WelcomeController < ApplicationController
   
   def about
     @flov = User.find_by_username('flov')
+    @hitchhikers_with_trips = User.all.collect{|user| user unless user.trips.nil? }.compact
   end
 end
