df8c11d730347919aa1de28c664fce74380744df
flov <florian.vallen@gmail.com>
2011-03-22 17:07:55 +0100

    New Awesome Design !!!WOOOOOOOOOT!!!

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,15 +3,19 @@ class WelcomeController < ApplicationController
 
   def home
     @hitchhike = Hitchhike.random_item
+    #@chart_image_header = chart_image(Trip.all, nil, :three_dimensional => true, :color => 'FFAC63', :size => 'small')
+
     @chart_image = chart_image(Trip.all)
-
-    hitchhikes = Hitchhike.all
-    @hitchhike_size = hitchhikes.size
-    @story_size = hitchhikes.collect{|hh| hh.story}.flatten.compact.delete_if{|x|x==''}.size
-    @hitchhiked_km = Trip.all.collect{|t| t.distance}.flatten.compact.sum / 1000
-
+    @trips = Trip.all.paginate :per_page => 5, :page => 1
+    @trip_size = @trips.size
+    @country_size = @trips.map(&:from_country).uniq.size
+    @active_hitchhikers = @trips.map(&:user).uniq.size
+    @hitchhike_size = Hitchhike.all.size
+    @story_size = Hitchhike.all.collect{|hh| hh.story}.flatten.compact.delete_if{|x|x==''}.size
+    @photo_size = Hitchhike.with_photo.size
+    @hitchhiked_km = @trips.collect{|t| t.distance}.flatten.compact.sum / 1000
     @hitchhikes_with_story = Hitchhike.with_story.order("updated_at DESC").paginate :per_page => 5, :page => params[:page]
-    @hitchhikers = User.order("created_at DESC").paginate :per_page => 5, :page => params[:page]
+    @hitchhikers = User.order("created_at DESC").paginate :per_page => 5, :page => 1
   end
   
   def about
--- app/models/hitchhike.rb
+++ app/models/hitchhike.rb
@@ -14,6 +14,7 @@ class Hitchhike < ActiveRecord::Base
   
   # scope :not_empty, where("photo_file_name IS NOT NULL OR title IS NOT NULL OR story IS NOT NULL OR duration IS NOT NULL OR waiting_time IS NOT NULL")
   scope :not_empty, where("story IS NOT NULL AND story <> '' OR photo_file_name IS NOT NULL")
+  scope :with_photo, where("photo_file_name IS NOT NULL")
   scope :with_story, where("story IS NOT NULL AND story <> ''")
   
   def to_s
