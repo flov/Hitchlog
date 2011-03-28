fa475b5ca69bcac1b1c952ffda3b22ffbe94051b
flov <florian.vallen@gmail.com>
2011-03-28 02:27:02 +0200

    Feature: route countries with distance in gmaps.rb Gmaps plugin now in own repository in github

--- lib/gmaps.rb
+++ lib/gmaps.rb
@@ -1,5 +1,5 @@
 module Gmaps
-  def Gmaps::distance(from='', to='')
+  def Gmaps::distance(from='', to='',via='')
     # Possible Values for result['status] 
     # "OK" indicates that no errors occurred; the address was successfully parsed and at least one geocode was returned.
     # "ZERO_RESULTS" indicates that the geocode was successful but returned no results. This may occur if the geocode was passed a non-existent address or a latlng in a remote location.
@@ -8,6 +8,7 @@ module Gmaps
     # "INVALID_REQUEST" generally indicates that the query (address or latlng) is missing.
     # "OFFLINE" indicates that you are not online
     url = "http://maps.googleapis.com/maps/api/directions/json?origin=#{CGI::escape(from)}&destination=#{CGI::escape(to)}&sensor=false"
+    #url = "http://maps.googleapis.com/maps/api/directions/json?origin=Amsterdam&waypoints=Antwerpen|Lille&destination=Duisburg&sensor=false"
     begin
       data = Net::HTTP.get_response(URI.parse(url)).body
       # we convert the returned JSON data to native Ruby hash
@@ -44,7 +45,7 @@ module Gmaps
     end
 
     if result['status'] == "OK"
-      result['results'].first['formatted_address']
+      result['results'][0]['formatted_address']
     else
       "unknown"
     end
@@ -62,10 +63,42 @@ module Gmaps
 
     if result['status'] == "OK"
       country = ''
-      result['results'].first['address_components'].each{|address| country = address['long_name'] if address['types'].include?("country")}
+      result['results'][0]['address_components'].each{|address| country = address['long_name'] if address['types'].include?("country")}
       country
     else
       "unknown"
+    end
+  end
+  
+  def Gmaps::countries(from, to)
+    url = "http://maps.googleapis.com/maps/api/directions/json?origin=#{CGI::escape(from)}&destination=#{CGI::escape(to)}&sensor=false"
+    begin
+      data = Net::HTTP.get_response(URI.parse(url)).body
+      # we convert the returned JSON data to native Ruby hash
+      result = JSON.parse(data)
+    rescue Exception => e
+      # Should only happen when used offline. Should not happen if online
+      result = {'status' => 'OFFLINE'}
+    end
+    if result['status'] == 'OK' # compute distance
+      countries = [[Gmaps.country(from), nil]]
+      distance = 0
+      result['routes'][0]['legs'][0]['steps'].each_with_index do |step, i|
+        distance += step['distance']['value']
+        if step['html_instructions'].match(/Entering [A-Z]/)
+          nokogiri = Nokogiri::HTML(step['html_instructions'])
+          country = nokogiri.search('div').last.content.sub('Entering ','')
+          countries[-1][1] = distance
+          countries << [country, nil]
+          distance = 0
+        end
+        if i == result['routes'][0]['legs'][0]['steps'].size-1
+          countries[-1][1] = distance
+        end
+      end 
+      countries
+    else
+      result['status']
     end
   end
 
@@ -80,7 +113,7 @@ module Gmaps
     end
     if result['status'] == "OK"
       country = ''
-      result['results'].first['address_components'].each do |address|
+      result['results'][0]['address_components'].each do |address|
         country = address['long_name'] if address['types'].include?("locality") and address['types'].include?("political")
         break unless country.blank?
       end
