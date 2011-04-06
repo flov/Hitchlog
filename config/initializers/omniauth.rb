586f14a22f243ab2da54215be2d593a0061c3387
flov <florian.vallen@gmail.com>
2011-04-06 17:28:35 +0200

    FIX: cucumber working again

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do  
   #provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'  
-  provider :facebook, Hitchlog::Application.config.facebook_id, Hitchlog::Application.config.facebook_secret, :scope => 'email,offline_access'
+  provider :facebook,
+           Hitchlog::Application.config.facebook_id,
+           Hitchlog::Application.config.facebook_secret,
+           :scope => 'email,offline_access'
 end  
