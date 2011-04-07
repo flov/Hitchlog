78730b915c26bbd358659c4f2bae70f891ae09cf
flov <florian.vallen@gmail.com>
2011-04-07 18:03:00 +0200

    FIX: New version of devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -34,17 +34,6 @@ Devise.setup do |config|
   # The realm used in Http Basic Authentication
   # config.http_authentication_realm = "Application"
 
-  # ==> Configuration for :database_authenticatable
-  # For bcrypt, this is the cost for hashing the password and defaults to 10. If
-  # using other encryptors, it sets how many times you want the password re-encrypted.
-  config.stretches = 10
-
-  # Define which will be the encryption algorithm. Devise also supports encryptors
-  # from others authentication tools as :clearance_sha1, :authlogic_sha512 (then
-  # you should set stretches above to 20 for default behavior) and :restful_authentication_sha1
-  # (then you should set stretches to 10, and copy REST_AUTH_SITE_KEY to pepper)
-  config.encryptor = :bcrypt
-
   # Setup a pepper to generate the encrypted password.
   config.pepper = "e61e2fa1aeb5895fbfbbb8c45bb282d9bda8f09dfeadc9afaeef950586b5bfd40b8414692524d494f66947d28e24a13f315a6d8b7c88194036c9e70d1dc52231"
 
