6cdfb4ebcdf75d1bd5417852e1f2e806a1e11419
flov <florian.vallen@gmail.com>
2011-04-27 03:13:01 +0200

    Feature: Facebook Button

--- app/helpers/omniauth_helper.rb
+++ app/helpers/omniauth_helper.rb
@@ -4,6 +4,11 @@ module OmniauthHelper
     link_to "Sign up with Facebook", user_omniauth_authorize_path(:facebook), :id => 'facebook_authentication'
   end
 
+  def facebook_button
+    #link_to "Sign up with Facebook", "/auth/facebook", :id => 'facebook_authentication'
+    link_to "Sign up with Facebook", user_omniauth_authorize_path(:facebook), :id => 'facebook_button'
+  end
+  
   def facebook_login
     #link_to "Login with Facebook", "/auth/facebook", :id => 'facebook_login'
     link_to "Login with Facebook", user_omniauth_authorize_path(:facebook), :id => 'facebook_authentication'
