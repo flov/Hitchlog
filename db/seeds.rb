932c3d0c6491ed6f2aa3b7ecda2b4684f1d58758
flov <florian.vallen@gmail.com>
2011-06-20 17:41:53 +0200

    seeds not working for the moment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,32 @@
-puts <<-EOS
-This will create some example users and a pre-populated project for development.
-EOS
+#puts <<-EOS
+#This will create some example users and a pre-populated project for development.
+#EOS
 
-def seed_data
-  puts "Seeding Users"
-  users = %w(Flov Supertramp).collect do |username|
-    user = User.find_by_username(username) || User.create!(
-                        :username => username,
-                        :password => 'adminadmin',
-                        :password_confirmation => 'adminadmin',
-                        :email    => "#{username}@hitchlog.com")
-  end
+#def seed_data
+  #puts "Seeding Users"
+  #users = %w(Flov Supertramp).collect do |username|
+    #user = User.find_by_username(username) || User.create!(
+                        #:username => username,
+                        #:password => 'adminadmin',
+                        #:password_confirmation => 'adminadmin',
+                        #:email    => "#{username}@hitchlog.com")
+  #end
 
-  trips = [%w(Barcelona Valencia 09/02/2011 2 1),
-           %w(Rome Milano 02/03/2010 4 1),
-           %w(Pontferrada Barcelona 09/05/2010 4 2)].collect do |from,to,start,rides,travelling_with|
-    User.find_by_username("flov").trips.build(
-      "from" => "Barcelona",
-      "to"   => "Valencia", 
-      "start"=> "#{start} 06:00", 
-      "end"  => "#{start} 13:00", 
-      "rides"=> rides, 
-      "travelling_with" => travelling_with, 
-      "money_spent" => "5")
-  end
+  #trips = [%w(Barcelona Valencia 09/02/2011 2 1),
+           #%w(Rome Milano 02/03/2010 4 1),
+           #%w(Pontferrada Barcelona 09/05/2010 4 2)].collect do |from,to,start,rides,travelling_with|
+    #User.find_by_username("flov").trips.build(
+      #"from" => "Barcelona",
+      #"to"   => "Valencia", 
+      #"start"=> "#{start} 06:00", 
+      #"end"  => "#{start} 13:00", 
+      #"rides"=> rides, 
+      #"travelling_with" => travelling_with, 
+      #"money_spent" => "5")
+  #end
 
   
-end
+#end
 
 
-seed_data
+#seed_data
