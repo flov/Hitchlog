fc8f08f9a329f7d7a7744881f2bce0cdaeb2aa64
flov <florian.vallen@gmail.com>
2011-09-03 13:15:29 +0300

    removed cucumber features

--- features/step_definitions/ride_steps.rb
+++
@@ -1,8 +0,0 @@
-Given /^there is a ride from ([^\"]*)$/ do |username|
-  user = User.find_by_username(username)
-  a = Factory.create(:ride)
-end
-
-Then /^I should see the hitchhike of Alexander but not the other one$/ do
-  pending # express the regexp above with the code you wish you had
-end
