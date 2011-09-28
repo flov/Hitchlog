ce3aee266217a1e251f0265d9fde4f859a66295c
flov <florian.vallen@gmail.com>
2011-09-28 11:18:29 +0300

    Migrations for Geocoding of ip ~> address

--- db/migrate/20110913100919_create_sign_in_addresses.rb
+++ db/migrate/20110913100919_create_sign_in_addresses.rb
@@ -1,5 +1,6 @@
 class CreateSignInAddresses < ActiveRecord::Migration
   def self.up
+    drop_table :sign_in_addresses
     create_table :sign_in_addresses do |t|
       t.string :city
       t.string :country_code
--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(:version => 20110914121958) do
     t.string  "origin"
     t.integer "ride_id"
     t.integer "age"
-    t.string  "gender",     :default => "male"
+    t.string  "gender"
   end
 
   create_table "rails_admin_histories", :force => true do |t|
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(:version => 20110914121958) do
     t.integer  "item"
     t.string   "table"
     t.integer  "month",      :limit => 2
-    t.integer  "year",       :limit => 5
+    t.integer  "year",       :limit => 8
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -53,14 +53,17 @@ ActiveRecord::Schema.define(:version => 20110914121958) do
 
   create_table "rides", :force => true do |t|
     t.string   "title"
+    t.string   "mission"
     t.string   "photo_file_name"
     t.string   "photo_content_type"
     t.string   "photo_file_size"
     t.string   "photo_updated_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
     t.text     "story"
     t.integer  "waiting_time"
+    t.datetime "date"
     t.integer  "trip_id"
     t.float    "duration"
     t.integer  "number"
@@ -128,6 +131,7 @@ ActiveRecord::Schema.define(:version => 20110914121958) do
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "",     :null => false
     t.string   "encrypted_password",   :limit => 128, :default => "",     :null => false
+    t.string   "password_salt",                       :default => "",     :null => false
     t.string   "reset_password_token"
     t.string   "remember_token"
     t.datetime "remember_created_at"
