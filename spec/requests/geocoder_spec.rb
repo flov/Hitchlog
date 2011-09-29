6d718ce16326f7b9731d8717822261aed0e0e35a
flov <florian.vallen@gmail.com>
2011-09-29 12:43:15 +0300

    switched styles for big input fields to fancy_form class

--- spec/requests/geocoder_spec.rb
+++ spec/requests/geocoder_spec.rb
@@ -2,26 +2,16 @@ require 'spec_helper'
 
 describe "Nearby Hitchhikers" do
   describe "Sign in and see nearby hitchhikers" do
-    xit "should display nearby hitchhikers if there are some" do
-      user1  = Factory(:user)
-      user2  = Factory(:user)
-      visit new_user_session_path
-      fill_in "Username", :with => user1.username
-      fill_in "Password", :with => 'password'
-      click_button "Sign in"
-      visit root_path
-      page.should have_content 'Nearby hitchhikers'
-    end
-
-    it "should not display nearby hitchhikers if there are none" do
+    it "should not display nearby hitchhikers" do
       user   = Factory :user
       user2  = Factory :user
       visit new_user_session_path
       fill_in "Username", :with => user.username
       fill_in "Password", :with => 'password'
       click_button "Sign in"
+      page.should have_content 'Nearby hitchhikers'
       visit root_path
-      page.should_not have_content 'Nearby hitchhikers'
+      page.should have_content 'Nearby hitchhikers'
     end
   end
 end
