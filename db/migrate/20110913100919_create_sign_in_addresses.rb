7e59203028f8908a5f0b3a2ab69c437bb707b1a7
flov <florian.vallen@gmail.com>
2011-09-29 15:57:34 +0300

    Fixed Migrations for Sign in  address

--- db/migrate/20110913100919_create_sign_in_addresses.rb
+++ db/migrate/20110913100919_create_sign_in_addresses.rb
@@ -1,6 +1,5 @@
 class CreateSignInAddresses < ActiveRecord::Migration
   def self.up
-    drop_table :sign_in_addresses
     create_table :sign_in_addresses do |t|
       t.string :city
       t.string :country_code
