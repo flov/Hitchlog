f7e3140eef4fa7f6a132902981594cf2457f3905
Florian Vallen <florian.vallen@gmail.com>
2011-12-16 19:14:06 +0530

    Geocoder Spec

--- spec/requests/geocoder_spec.rb
+++ spec/requests/geocoder_spec.rb
@@ -1,17 +1,29 @@
 require 'spec_helper'
 
 describe "Nearby Hitchhikers" do
-  describe "Sign in and see nearby hitchhikers" do
-    it "should not display nearby hitchhikers" do
-      user   = Factory :user
-      user2  = Factory :user
+  # describe "Sign in and see nearby hitchhikers" do
+  #   it "should display nearby hitchhikers" do
+  #     user   = Factory :user
+  #     user2  = Factory :user
+  #     visit new_user_session_path
+  #     fill_in "Username", :with => user.username
+  #     fill_in "Password", :with => 'password'
+  #     click_button "Sign in"
+  #     page.should have_content 'Nearby hitchhikers'
+  #     visit root_path
+  #     page.should have_content 'Nearby hitchhikers'
+  #   end
+  # end
+
+  describe "last login" do
+    it "should dissplay nnew last signed in address" do
+      user   = Factory :berlin_user
       visit new_user_session_path
       fill_in "Username", :with => user.username
       fill_in "Password", :with => 'password'
       click_button "Sign in"
-      page.should have_content 'Nearby hitchhikers'
-      visit root_path
-      page.should have_content 'Nearby hitchhikers'
+      # save_and_open_page
+      visit users_path
     end
   end
 end
