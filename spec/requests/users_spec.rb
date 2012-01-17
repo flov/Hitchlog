ec7575f74f844da9f090a7142955dd3d2ae15d0b
Florian Vallen <florian.vallen@gmail.com>
2012-01-17 16:50:24 +0530

    Edit Profile added

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -6,21 +6,31 @@ describe "users" do
   end
 
   describe "GET /users/edit" do
-    before do
-      visit new_user_session_path
-      fill_in "Username", :with => @user.username
-      fill_in "Password", :with => 'password'
-      click_button "Sign in"
-      visit user_path(@user)
-      click_link "Edit Profile"
+    context "logged in" do
+      before do
+        visit new_user_session_path
+        fill_in "Username", :with => @user.username
+        fill_in "Password", :with => 'password'
+        click_button "Sign in"
+        visit user_path(@user)
+        click_link "Edit Profile"
+      end
+
+      it "displays fields for editing profil" do
+        fill_in "About you", :with => 'new_username'
+        select  "Female", :from => "Gender"
+        fill_in "CS user", :with => "flov"
+        page.should have_content   "Avatar"
+        page.should have_content   "Hitchlog only supports avatars through Gravatar."
+        click_button "Update User"
+      end
     end
 
-    xit "displays fields for editing profil" do
-      page.should have_field     "About you" 
-      page.should have_select    "Gender" 
-      page.should have_field     "CS User" 
-      page.should have_content   "Avatar" 
-      page.should have_content   "Hitchlog only supports profile images through gravatar.com. If you want to upload your own image, please upload your own photo on gravatar.com." 
+    context "logged out" do
+      it "should not see Edit Profile" do
+        visit user_path(@user)
+        page.should_not have_content  "Edit Profile" 
+      end
     end
   end
 end
