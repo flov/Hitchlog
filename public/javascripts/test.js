7001e824779af95e637a29c05fcec1c58d84a5b5
Florian Vallen <florian.vallen@gmail.com>
2012-01-26 18:44:21 +0530

    not needed js file

--- public/javascripts/test.js
+++
@@ -1,7 +0,0 @@
-/* DO NOT MODIFY. This file was compiled Wed, 08 Jun 2011 18:55:12 GMT from
- * /Users/florianvallen/code/hitchlog/app/coffeescripts/test.coffee
- */
-
-(function() {
-
-}).call(this);
