bcb71489588184263356d3d3450111680a4ecb74
Florian Vallen <florian.vallen@gmail.com>
2012-02-11 15:30:44 +0530

    Mailer: initialization

---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+  :address              => Rails.configuration.smtp_settings[:address],
+  :port                 => Rails.configuration.smtp_settings[:port],
+  :domain               => Rails.configuration.smtp_settings[:domain],
+  :user_name            => Rails.configuration.smtp_settings[:user_name],
+  :password             => Rails.configuration.smtp_settings[:password],
+  :authentication       => Rails.configuration.smtp_settings[:authentication],
+  :enable_starttls_auto => true
+}
