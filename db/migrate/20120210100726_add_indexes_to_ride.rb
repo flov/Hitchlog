6519816bc2142980e278db1b84b337a65e4f475f
Florian Vallen <florian.vallen@gmail.com>
2012-02-18 01:52:58 +0545

    Migrationfix: rides don't have a user_id

--- db/migrate/20120210100726_add_indexes_to_ride.rb
+++ db/migrate/20120210100726_add_indexes_to_ride.rb
@@ -3,6 +3,5 @@ class AddIndexesToRide < ActiveRecord::Migration
     add_index :rides, :gender
     add_index :rides, :experience
     add_index :rides, :trip_id
-    add_index :rides, :user_id
   end
 end
