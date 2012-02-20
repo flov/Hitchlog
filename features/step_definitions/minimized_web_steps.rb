0a4200bebdbe0aa1f30b083e8fff5a1385aedd80
Florian Vallen <florian.vallen@gmail.com>
2012-02-20 11:27:18 +0545

    Tests: my own web steps and paths.rb

---
+++ features/step_definitions/minimized_web_steps.rb
@@ -0,0 +1,12 @@
+Then /^I should be on (.+)$/ do |page_name|
+  current_path = URI.parse(current_url).path
+  current_path.should == path_to(page_name)
+end
+
+Then /^I should see "([^"]*)"$/ do |text|
+  page.should have_content(text)
+end
+
+When /^I go to (.+)$/ do |page_name|
+  visit path_to(page_name)
+end
