3d0cf814e5b59db3f7e8dc3de3ff579d51f48564
Florian Vallen <florian.vallen@gmail.com>
2012-05-28 09:38:08 +0545

    ts# This is a combination of 2 commits.
    Added Profile image to navigation bar

--- config/initializers/gravatar_image_tag.rb
+++ config/initializers/gravatar_image_tag.rb
@@ -1,6 +1,6 @@
 # config/initializers/gravatar_image_tag.rb
 GravatarImageTag.configure do |config|
-  config.default_image = :monsterid
+  config.default_image = :identicon
   config.filetype      = nil   # Set this if you require a specific image file format ['gif', 'jpg' or 'png'].  Gravatar's default is png
   config.rating        = nil   # Set this if you change the rating of the images that will be returned ['G', 'PG', 'R', 'X']. Gravatar's default is G
   config.size          = nil   # Set this to globally set the size of the gravatar image returned (1..512). Gravatar's default is 80
