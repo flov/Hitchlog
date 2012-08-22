89bf3f3c46899a431cd9374b2bda957c77eb1e84
Florian Vallen <florian.vallen@gmail.com>
2012-08-22 21:09:44 +1000

    Refactored Cucumber Features

---
+++ features/step_definitions/race_steps.rb
@@ -0,0 +1,19 @@
+Given /^a hitchhiking race called "([^"]*)" from "([^"]*)" to "([^"]*)"$/ do |arg1, arg2, arg3|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^"([^"]*)" and "([^"]*)" belong to a hitchhiking team called "([^"]*)"$/ do |arg1, arg2, arg3|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^the team "([^"]*)" is participating in the race "([^"]*)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see a map with a route from "([^"]*)" to "([^"]*)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see that team "([^"]*)" is participating in the race "([^"]*)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
