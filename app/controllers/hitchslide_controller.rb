92949823faa5e50e848b2eea9467ec7755276003
Florian Vallen <florian.vallen@gmail.com>
2012-12-07 14:57:36 +1000

    Hitchslide working again

--- app/controllers/hitchslide_controller.rb
+++ app/controllers/hitchslide_controller.rb
@@ -1,31 +1,31 @@
 class HitchslideController < ApplicationController
-  def next
-    @ride = Ride.where('photo_file_name is not null')
-                    .where("id > #{params[:id].to_i}")
-                    .order(:id).first
+  #def next
+    #@ride = Ride.where('photo_file_name is not null')
+                    #.where("id > #{params[:id].to_i}")
+                    #.order(:id).first
 
-    if @ride.nil?
-      @ride = Ride.where('photo_file_name is not null').order(:id).first
-    end
+    #if @ride.nil?
+      #@ride = Ride.where('photo_file_name is not null').order(:id).first
+    #end
 
-    respond_to do |format|
-      format.json { render "rides/show", formats: [:json] }
-    end
-  end
+    #respond_to do |format|
+      #format.json { render "rides/show", formats: [:json] }
+    #end
+  #end
 
-  def prev
-    @ride = Ride.where('photo_file_name is not null')
-                .where("id < #{params[:id].to_i}")
-                .order(:id).last
+  #def prev
+    #@ride = Ride.where('photo_file_name is not null')
+                #.where("id < #{params[:id].to_i}")
+                #.order(:id).last
 
-    if @ride.nil?
-      @ride = Ride.where('photo_file_name is not null').order(:id).last
-    end
+    #if @ride.nil?
+      #@ride = Ride.where('photo_file_name is not null').order(:id).last
+    #end
 
-    respond_to do |format|
-      format.json { render "rides/show", formats: [:json] }
-    end
-  end
+    #respond_to do |format|
+      #format.json { render "rides/show", formats: [:json] }
+    #end
+  #end
 
   def random
     respond_to do |format|
