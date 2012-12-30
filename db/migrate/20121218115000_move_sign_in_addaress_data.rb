09886b4831230519b850b42e774c677de86d07c2
Florian Vallen <florian.vallen@gmail.com>
2012-12-30 22:17:12 +1000

    hotfix: migration fix

--- db/migrate/20121218115000_move_sign_in_addaress_data.rb
+++ db/migrate/20121218115000_move_sign_in_addaress_data.rb
@@ -1,9 +1,15 @@
+class SignInAddress < ActiveRecord::Base
+  belongs_to :user
+end
+
 class MoveSignInAddaressData < ActiveRecord::Migration
   def up
     SignInAddress.all.each do |sign_in_address|
-      sign_in_address.user.update_column(:city, sign_in_address.city)
-      sign_in_address.user.update_column(:country, sign_in_address.country)
-      sign_in_address.user.update_column(:country_code, sign_in_address.country_code)
+      unless sign_in_address.user.nil?
+        sign_in_address.user.update_column(:city, sign_in_address.city)
+        sign_in_address.user.update_column(:country, sign_in_address.country)
+        sign_in_address.user.update_column(:country_code, sign_in_address.country_code)
+      end
     end
   end
 
