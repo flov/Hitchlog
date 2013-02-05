7e113b9f62ab8df6c64dfedd01069e4c3a641030
Florian Vallen <florian.vallen@gmail.com>
2013-02-05 15:06:49 +1100

    no commented code in git repo...

--- app/controllers/hitchslide_controller.rb
+++ app/controllers/hitchslide_controller.rb
@@ -1,32 +1,4 @@
 class HitchslideController < ApplicationController
-  #def next
-    #@ride = Ride.where('photo_file_name is not null')
-                    #.where("id > #{params[:id].to_i}")
-                    #.order(:id).first
-
-    #if @ride.nil?
-      #@ride = Ride.where('photo_file_name is not null').order(:id).first
-    #end
-
-    #respond_to do |format|
-      #format.json { render "rides/show", formats: [:json] }
-    #end
-  #end
-
-  #def prev
-    #@ride = Ride.where('photo_file_name is not null')
-                #.where("id < #{params[:id].to_i}")
-                #.order(:id).last
-
-    #if @ride.nil?
-      #@ride = Ride.where('photo_file_name is not null').order(:id).last
-    #end
-
-    #respond_to do |format|
-      #format.json { render "rides/show", formats: [:json] }
-    #end
-  #end
-
   def random
     respond_to do |format|
       format.json { render "rides/show", ride: random_photo_ride, formats: [:json] }
