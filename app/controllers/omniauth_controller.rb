e01d42c000e30a8ca6290d226a696aa0248fa551
Florian Vallen <florian.vallen@gmail.com>
2013-02-21 17:49:47 +1100

    important Hotfix: Facebook authentication working

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -15,7 +15,7 @@ class OmniauthController < ApplicationController
       @user = User.new(email: data['email'],
                        gender: data['gender'],
                        password: Devise.friendly_token[0,20],
-                       username: facebook_username(data['extra']['raw_info'])) 
+                       username: facebook_username(data)) 
       @user.authentications.build(provider: access_token['provider'],
                                   uid: access_token['uid'])
       @user.save!
@@ -40,12 +40,33 @@ class OmniauthController < ApplicationController
 
   def facebook_username(hash)
     # A user does not need to have a nickname configured:
-    # nickname: profile.php?id=100002244415511
-    if hash['nickname'].include? '?'
+    # username: profile.php?id=100002244415511
+    if hash['username'].include? '?'
       hash['first_name']
     else
-      hash['nickname'].gsub('.', '_')
+      hash['username'].gsub('.', '_')
     end
   end
 end
+
+
+#<OmniAuth::AuthHash credentials=#<Hashie::Mash expires=true
+#expires_at=1366612953
+#token="AAAEu9KV9HBUBAGkuut7xArGeFoGgJtjlkYozvjawupT2L3ZAZAMywvHygw5JdfUPXgEOXTRq6KaxJMZAjYKtXYu5fOAIzFrZA4LZAbjvBRAZDZD">
+#extra=#<Hashie::Mash raw_info=#<Hashie::Mash email="florian.vallen@gmail.com"
+#favorite_teams=[#<Hashie::Mash id="401872513207416" name="Reisegruppe
+#Veteran">] first_name="Florian" gender="male" id="1011496368"
+#languages=[#<Hashie::Mash id="106059522759137" name="English">, #<Hashie::Mash
+#id="105673612800327" name="German">, #<Hashie::Mash id="110343528993409"
+#name="Spanish">] last_name="Vallen" link="http://www.facebook.com/fvallen"
+#locale="en_US" location=#<Hashie::Mash id="116190411724975" name="Melbourne,
+#Victoria, Australia"> name="Florian Vallen" timezone=11
+#updated_time="2013-02-06T15:48:14+0000" username="fvallen" verified=true>>
+#info=#<OmniAuth::AuthHash::InfoHash email="florian.vallen@gmail.com"
+                                    #first_name="Florian"
+                                    #image="http://graph.facebook.com/1011496368/picture?type=square"
+                          #last_name="Vallen" location="Melbourne, Victoria, Australia" name="Florian
+                        #Vallen" nickname="fvallen" urls=#<Hashie::Mash
+                        #Facebook="http://www.facebook.com/fvallen"> verified=true> provider="facebook"
+#uid="1011496368">
 
