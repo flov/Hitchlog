ffd71da7d77f40bda080e83684e9620ed0efd48d
Florian Vallen <florian.vallen@gmail.com>
2013-02-26 12:30:30 +1100

    omniauth mock for Facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,42 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, Hitchlog::Application.config.facebook_id,
                       Hitchlog::Application.config.facebook_secret,
                       scope: 'email'
+
+  OmniAuth.config.add_mock(:facebook, {
+    :provider => 'facebook',
+    :uid => '1234567',
+    :info => {
+      :nickname => 'jbloggs',
+      :email => 'joe@bloggs.com',
+      :name => 'Joe Bloggs',
+      :first_name => 'Joe',
+      :last_name => 'Bloggs',
+      :image => 'http://graph.facebook.com/1234567/picture?type=square',
+      :urls => { :Facebook => 'http://www.facebook.com/jbloggs' },
+      :location => 'Palo Alto, California',
+      :verified => true
+    },
+    :credentials => {
+      :token => 'ABCDEF...', # OAuth 2.0 access_token, which you may wish to store
+      :expires_at => 1321747205, # when the access token expires (it always will)
+      :expires => true # this will always be true
+    },
+    :extra => {
+      :raw_info => {
+        :id => '1234567',
+        :name => 'Joe Bloggs',
+        :first_name => 'Joe',
+        :last_name => 'Bloggs',
+        :link => 'http://www.facebook.com/jbloggs',
+        :username => 'jbloggs',
+        :location => { :id => '123456789', :name => 'Palo Alto, California' },
+        :gender => 'male',
+        :email => 'joe@bloggs.com',
+        :timezone => -8,
+        :locale => 'en_US',
+        :verified => true,
+        :updated_time => '2011-11-11T06:21:03+0000'
+      }
+    }
+  })
 end
