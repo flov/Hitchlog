adea373351e95ebd79384acf4e275b162bcb2ac2
Florian Vallen <florian.vallen@gmail.com>
2013-05-13 02:16:59 +1000

    show current active navigation

---
+++ app/helpers/navigation_helper.rb
@@ -0,0 +1,25 @@
+module NavigationHelper
+  def home_active?
+    if 'welcome#bootstrap' == "#{controller.controller_name}##{controller.action_name}"
+      'active'
+    end
+  end
+
+  def trips_active?
+    if 'trips' == controller.controller_name
+      'active'
+    end
+  end
+
+  def users_active?
+    if 'users' == controller.controller_name
+      'active'
+    end
+  end
+
+  def about_active?
+    if 'welcome#about' == "#{controller.controller_name}##{controller.action_name}"
+      'active'
+    end
+  end
+end
