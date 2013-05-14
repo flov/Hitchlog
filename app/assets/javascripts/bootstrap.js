ab4f53d9d3869d5d15f3c26939586b00c0a8df6b
Florian Vallen <florian.vallen@gmail.com>
2013-05-14 22:07:37 +1000

    bootstrap js alert

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -6,6 +6,8 @@
 //= require twitter/bootstrap/tooltip
 //= require twitter/bootstrap/collapse
 //= require twitter/bootstrap/dropdown
+//= require twitter/bootstrap/tab
+//= require twitter/bootstrap/alert
 //
 
 
@@ -13,3 +15,6 @@
 $("img.tip").tooltip()
 // Dropdown Settings:
 $('.dropdown-toggle').dropdown()
+// Alert init
+$(".alert").alert()
+
