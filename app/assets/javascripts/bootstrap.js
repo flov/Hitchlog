d8feb793c8085cf9d8b2c36dfd97d13248de6d2e
Florian Vallen <florian.vallen@gmail.com>
2013-05-26 11:20:03 +1000

    updated VCR

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -8,6 +8,8 @@
 //= require twitter/bootstrap/dropdown
 //= require twitter/bootstrap/tab
 //= require twitter/bootstrap/alert
+//= require twitter/bootstrap/modal
+//
 //= require vendor/jquery.observe_field.js
 //
 
