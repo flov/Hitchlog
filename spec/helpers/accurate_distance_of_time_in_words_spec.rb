453d5767fa01fa41e35754dcee666219688d5baa
Florian Vallen <florian.vallen@gmail.com>
2013-06-24 11:12:42 +0800

    Spec: accurate time in words

---
+++ spec/helpers/accurate_distance_of_time_in_words_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe "A more accurate distance_of_time_in_words" do
+  include AccurateDistanceOfTimeInWordsHelper
+
+  describe "accurate distance of time" do
+    [
+      [4.5.minutes, "4 minutes"],
+      [1.hour.to_i, "1 hour"],
+      [2.hours + 5.minutes, "2 hours and 5 minutes"],
+      [1.day + 2.hours + 30.minutes, "1 day, 2 hours, and 30 minutes"]
+    ].each do |number, result|
+      it "#{number} == #{result}" do
+        accurate_distance_of_time_in_words(number).should eql(result)
+      end
+    end
+  end
+end
