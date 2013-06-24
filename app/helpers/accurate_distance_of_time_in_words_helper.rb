8422df65a273e4d9cf2009ccb4a895e33c36c491
Florian Vallen <florian.vallen@gmail.com>
2013-06-24 11:08:01 +0800

    accurate distance of time in words: - e.g not about 4 hours, but 4 hours and 4 minutes

---
+++ app/helpers/accurate_distance_of_time_in_words_helper.rb
@@ -0,0 +1,23 @@
+module AccurateDistanceOfTimeInWordsHelper
+  def accurate_distance_of_time_in_words(seconds)
+    seconds = seconds.to_f
+    array = []
+
+    while(seconds > 0)
+      if seconds >= 1.day
+        days, seconds = seconds.divmod(1.day)
+        array << I18n.t('datetime.distance_in_words.x_days', count: days)
+      elsif seconds >= 1.hour
+        hours, seconds = seconds.divmod(1.hour)
+        array << I18n.t('datetime.distance_in_words.x_hours', count: hours)
+      elsif seconds >= 1.minute
+        minutes, seconds = seconds.divmod(1.minute)
+        array << I18n.t('datetime.distance_in_words.x_minutes', count: minutes)
+      else
+        seconds = 0
+      end
+    end
+
+    array.to_sentence
+  end
+end
