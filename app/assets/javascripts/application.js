b8702eeb21db8de57ec01a0a3069680edfc9a6a7
Florian Vallen <florian.vallen@gmail.com>
2013-06-28 17:39:13 +0800

    renaming bootstrap.js to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,25 @@
+//
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui
-//= require vendor/jquery-ui-timepicker-addon.js
-//= require vendor/jquery.tipsy.js
-//= require vendor/jquery.Jcrop.min.js
-//= require vendor/jquery.observe_field.js
-//= require vendor/jquery.nivo.slider.pack.js
-//= require vendor/showdown.js
+//= require jquery.ui.accordion
+//= require jquery.ui.tabs
 //
-//= require tipsy_settings.js
-//= require map
-//= require hitchmap
-//= require hitchslider
-//= require navigation
-//= require location
+//= require twitter/bootstrap/tooltip
+//= require twitter/bootstrap/collapse
+//= require twitter/bootstrap/dropdown
+//= require twitter/bootstrap/tab
+//= require twitter/bootstrap/alert
+//= require twitter/bootstrap/modal
+//
+//= require vendor/bootstrap-datetimepicker.js
+//= require vendor/jquery.observe_field.js
+//
+
+
+// Tooltip Settings:
+$(".tip").tooltip()
+// Dropdown Settings:
+$('.dropdown-toggle').dropdown()
+// Alert init
+$(".alert").alert()
+
