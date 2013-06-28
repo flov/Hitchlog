7b19933f8450cbab6ea30804e2df90938dfbcc87
Florian Vallen <florian.vallen@gmail.com>
2013-06-28 13:37:27 +0800

    removed old app layout and old app stylesheet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
 
   before_filter :set_locale
 
-  layout "bootstrap"
-
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
