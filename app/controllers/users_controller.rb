10709ef7f95d3b1041cf26802b4af78166f338af
Florian Vallen <florian.vallen@gmail.com>
2013-07-13 14:05:00 +0800

    pagination issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
   expose(:user)
   expose(:users) { User.order('current_sign_in_at DESC').paginate(page: params[:page], per_page: 20) }
-  expose(:trips) { build_search_trips(user.trips).paginate(page: params[:page]) }
 
   before_filter :authenticate_user!, only: [:edit, :mail_sent, :send_mail, :destroy, :update]
+
+  def show
+    @trips = user.trips.paginate(page: params[:page], per_page: 5)  
+  end
 
   def update
     @user = current_user
