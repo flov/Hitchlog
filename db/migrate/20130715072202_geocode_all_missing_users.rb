eb5f8d3b4943f772bdf75842f19d31206242c8c5
Florian Vallen <florian.vallen@gmail.com>
2013-07-16 22:07:22 +0700

    output for data migration

--- db/migrate/20130715072202_geocode_all_missing_users.rb
+++ db/migrate/20130715072202_geocode_all_missing_users.rb
@@ -3,10 +3,14 @@ class GeocodeAllMissingUsers < ActiveRecord::Migration
     User.where('users.lat is null').where('current_sign_in_ip is not null').all.each do |user|
       user.geocode
       user.reverse_geocode
+      puts "Updating #{user}:"
+      puts "lat #{user.lat}, lng #{user.lng}, city #{user.city}, country #{user.country}:"
       user.save
     end
     User.where('lat is not null').where('current_sign_in_ip is not null').all.each do |user|
       user.reverse_geocode
+      puts "Updating #{user}:"
+      puts "city #{user.city}, country #{user.country}:"
       user.save
     end
   end
