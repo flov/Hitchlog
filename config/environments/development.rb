36ecdbe34150d525859d897121d58317b975fe3f
Florian Vallen <florian.vallen@gmail.com>
2013-07-22 22:12:57 +0700

    staging environment and mail trap

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,12 +34,15 @@ Hitchlog::Application.configure do
   config.action_mailer.raise_delivery_errors = false
 	config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'hitchlog.dev' }
-	config.action_mailer.smtp_settings = {
-	  :user_name => ENV['MAILTRAP_USER'],
-	  :password =>  ENV['MAILTRAP_PASSWORD'],
-	  :address =>   ENV['MAILTRAP_HOST'],
-	  :port =>      ENV['MAILTRAP_PORT'],
-	  :authentication => :plain
-	}
+
+  config.after_initialize do
+    config.action_mailer.smtp_settings = {
+      :user_name => ENV['MAILTRAP_USER'],
+      :password =>  ENV['MAILTRAP_PASSWORD'],
+      :address =>   ENV['MAILTRAP_HOST'],
+      :port =>      ENV['MAILTRAP_PORT'],
+      :authentication => :plain
+    }
+  end
 end
 
