e1be388ab3d2ae1285a5a3d4fc6dad4850b88400
Florian Vallen <florian.vallen@gmail.com>
2013-07-26 19:49:12 +0700

    meta description optimization

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -5,30 +5,21 @@
 module LayoutHelper
   def title(page_title, show_title = true)
     content_for(:title) { page_title.to_s }
-    @show_title = show_title
   end
-  
+
   def meta_keywords(keywords = nil)
-    if keywords.nil?
-      "<meta name='keywords' content='hitchhiking,hitchhiker,blog,free travel,autostop,trampen' /> ".html_safe
-    else
-      "<meta name='keywords' content='#{keywords}' /> ".html_safe
+    content_for :head do
+      if keywords.nil?
+        "<meta name='keywords' content='hitchhiking,hitchhiker,blog,free travel,autostop,trampen' /> ".html_safe
+      else
+        "<meta name='keywords' content='#{keywords}' /> ".html_safe
+      end
     end
   end
-  
+
   def meta_description(description=nil)
-    "<meta name='description' content='#{I18n.t('meta.description')}'/>".html_safe
-  end
-
-  def show_title?
-    @show_title
-  end
-
-  def stylesheet(*args)
-    content_for(:head) { stylesheet_link_tag(*args) }
-  end
-
-  def javascript(*args)
-    content_for(:head) { javascript_include_tag(*args) }
+    content_for :head do
+      "<meta name='description' content='#{description}'/>".html_safe
+    end
   end
 end
