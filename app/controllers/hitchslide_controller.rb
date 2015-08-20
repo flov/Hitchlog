06e2b10f0803d07d1d49983d4ddde81491fb05d8
Florian Vallen <florian.vallen@gmail.com>
2015-08-20 16:11:41 +0200

    not needed controller

--- app/controllers/hitchslide_controller.rb
+++
@@ -1,7 +0,0 @@
-class HitchslideController < ApplicationController
-  def random
-    respond_to do |format|
-      format.json { render "rides/show", ride: random_photo_ride, formats: [:json] }
-    end
-  end
-end
