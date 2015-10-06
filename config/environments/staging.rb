abf079d6dc32dcbc920e2f884d3e7f18821352e3
Florian Vallen <florian.vallen@gmail.com>
2015-10-06 17:37:55 +0200

    changes config for staging environment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -5,15 +5,17 @@ Hitchlog::Application.configure do
   config.action_controller.perform_caching = true
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
+
   config.action_mailer.raise_delivery_errors = false
 
   config.i18n.fallbacks = true
 
   config.active_support.deprecation = :notify
-
   config.serve_static_assets = true
 
   config.assets.compress = true
+  config.assets.js_compressor = :uglifier
+  config.eager_load = true
 
   config.assets.compile = false
   config.assets.digest = true
