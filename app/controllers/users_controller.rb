53f79c4867c385a98fdf2af0534313863906a634
Florian Vallen <florian.vallen@gmail.com>
2015-10-08 00:19:17 +0200

    Fixes redirection of user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -74,11 +74,16 @@ class UsersController < ApplicationController
 
   def user_in_context
     if params[:id]
-      User.includes(trips: [:rides, :country_distances]).find_by_username(params[:id])
+      user = User.includes(trips: [:rides, :country_distances]).find_by_username(params[:id])
+      if user.nil?
+        redirect_to root_path, flash: { error: t('general.record_not_found')}
+      else
+        return user
+      end
     else
       User.new(params[:user])
     end
+
     rescue ActiveRecord::RecordNotFound
-      redirect_to root_path, flash: { error: t('general.record_not_found')}
   end
 end
