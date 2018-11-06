d91dd329f3bd9e8033d98d25545089b4dd239658
flov <florian.vallen@gmail.com>
2018-11-06 15:35:11 +0100

    adding google maps key to a horrible library that I once wrote many years ago that I shouldn't be using at all actually.
    Should be replaced by javascript and everything that happens here should
    happen when the form is being filled

--- lib/gmaps.rb
+++ lib/gmaps.rb
@@ -1,14 +1,13 @@
 module Gmaps
   def Gmaps::distance(from='', to='',via='')
-    # Possible Values for result['status] 
+    # Possible Values for result['status]
     # "OK" indicates that no errors occurred; the address was successfully parsed and at least one geocode was returned.
     # "ZERO_RESULTS" indicates that the geocode was successful but returned no results. This may occur if the geocode was passed a non-existent address or a latlng in a remote location.
     # "OVER_QUERY_LIMIT" indicates that you are over your quota.
     # "REQUEST_DENIED" indicates that your request was denied, generally because of lack of a sensor parameter.
     # "INVALID_REQUEST" generally indicates that the query (address or latlng) is missing.
     # "OFFLINE" indicates that you are not online
-    url = "http://maps.googleapis.com/maps/api/directions/json?origin=#{CGI::escape(from)}&destination=#{CGI::escape(to)}&sensor=false"
-    #url = "http://maps.googleapis.com/maps/api/directions/json?origin=Amsterdam&waypoints=Antwerpen|Lille&destination=Duisburg&sensor=false"
+    url = "http://maps.googleapis.com/maps/api/directions/json?key=#{ENV['GOOGLE_MAPS']}&origin=#{CGI::escape(from)}&destination=#{CGI::escape(to)}&sensor=false"
     begin
       data = Net::HTTP.get_response(URI.parse(url)).body
       # we convert the returned JSON data to native Ruby hash
@@ -19,7 +18,7 @@ module Gmaps
     end
     distance = 0
     if result['status'] == 'OK' # compute distance
-      result['routes'][0]['legs'].each{|leg| distance += leg['distance']['value']} 
+      result['routes'][0]['legs'].each{|leg| distance += leg['distance']['value']}
     elsif result['status'] == 'OFFLINE'
       distance = -1
     elsif result['status'] == 'OVER_QUERY_LIMIT'
@@ -35,7 +34,7 @@ module Gmaps
   end
   
   def Gmaps::formatted_address(address)
-    url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{CGI::escape(address)}&sensor=false"
+    url = "http://maps.googleapis.com/maps/api/geocode/json?key=#{ENV['GOOGLE_MAPS']}&address=#{CGI::escape(address)}&sensor=false"
     begin
       data = Net::HTTP.get_response(URI.parse(url)).body
       result = JSON.parse(data)
@@ -52,7 +51,7 @@ module Gmaps
   end
   
   def Gmaps::country(address)
-    url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{CGI::escape(address)}&sensor=false"
+    url = "http://maps.googleapis.com/maps/api/geocode/json?key=#{ENV['GOOGLE_MAPS']}&address=#{CGI::escape(address)}&sensor=false"
     begin
       data = Net::HTTP.get_response(URI.parse(url)).body
       result = JSON.parse(data)
@@ -103,7 +102,7 @@ module Gmaps
   end
 
   def Gmaps::city(address)
-    url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{CGI::escape(address)}&sensor=false"
+    url = "http://maps.googleapis.com/maps/api/geocode/json?key=#{ENV['GOOGLE_MAPS']}&address=#{CGI::escape(address)}&sensor=false"
     begin
       data = Net::HTTP.get_response(URI.parse(url)).body
       result = JSON.parse(data)
