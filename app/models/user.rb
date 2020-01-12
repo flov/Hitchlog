a9610515a5f893ef544ca9e07cc6ff7f0de2cd68
Florian Vallen <florian.vallen@gmail.com>
2020-01-12 14:53:48 +0100

    confirm user after authenticating with facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -208,7 +208,7 @@ class User < ActiveRecord::Base
     graph = Koala::Facebook::API.new(auth.credentials.token)
     fb_info = graph.get_object("me", {fields: "birthday,gender"})
 
-    User.where(provider: auth.provider, uid: auth.uid).first_or_create do |u|
+    user = User.where(provider: auth.provider, uid: auth.uid).first_or_create do |u|
       u.email            = auth.info.email
       u.gender           = fb_info["gender"]
       u.date_of_birth    = Date.strptime(fb_info["birthday"], '%m/%d/%Y')
@@ -225,8 +225,10 @@ class User < ActiveRecord::Base
       u.oauth_token      = auth.credentials.token
       u.oauth_expires_at = Time.at(auth.credentials.expires_at)
     end
+    user.confirm
+    user
   end
-  
+
   private
 
   def update_location_updated_at
