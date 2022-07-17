66b8d8402e87ee17950804e648afdb4209f656a4
Florian Vallen <florian.vallen@gmail.com>
2022-07-17 17:09:10 +0200

    Disabled confirmable for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
              'non-binary'].freeze
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :confirmable,
+         :recoverable, :rememberable, :trackable,
          :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
   has_many :rides, through: :trips
